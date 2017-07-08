6de58d00c646388d6a3d298b1f26c364eed9ca8d
Lachlan Eagling <lochie.eagling@gmail.com>
2017-07-08 19:52:04 +1000

    Refactor MainActivity to new package structure

--- app/src/main/java/com/bookbase/bookbase/activities/MainActivity.java
+++ app/src/main/java/com/bookbase/bookbase/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.bookbase.bookbase;
+package com.bookbase.bookbase.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bookbase.bookbase.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Test comment.
     }
 }
