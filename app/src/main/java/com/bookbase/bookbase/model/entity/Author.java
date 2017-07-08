70bd42bf36e01a827986818498e76e7691ff9cce
Lachlan Eagling <lochie.eagling@gmail.com>
2017-07-08 22:35:04 +1000

    Fix compile errors
    Fixed following issues when compiling.
    
    - Multiple constructors causing conflict with Room API.
    - Missing getter/setter for uid field.

--- app/src/main/java/com/bookbase/bookbase/model/entity/Author.java
+++ app/src/main/java/com/bookbase/bookbase/model/entity/Author.java
@@ -2,6 +2,7 @@ package com.bookbase.bookbase.model.entity;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
 
 @Entity
@@ -16,17 +17,23 @@ public class Author {
     @ColumnInfo(name = "name_family")
     private String lastName;
 
+    @Ignore
     private Author(){
         // Privatise default constructor to force initialisation of required fields.
     }
 
-    private Author(String firstName, String lastName){
+    public Author(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    public int getUid(){ return uid; }
     public String getFirstName(){ return firstName; }
     public String getLastName() { return lastName; }
+
+    public  void setUid(int uid){
+        this.uid = uid;
+    }
 
     public void setFirstName(String firstName){
         this.firstName = firstName;
