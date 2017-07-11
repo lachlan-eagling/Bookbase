6b6b623ed58b1a2b0fe21b481a1469549ba66f5a
Lachlan Eagling <lochie.eagling@gmail.com>
2017-07-11 21:02:00 +1000

    Implement onClick logic for nav drawer

--- app/src/main/java/com/bookbase/bookbase/activities/MainActivity.java
+++ app/src/main/java/com/bookbase/bookbase/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.bookbase.bookbase.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,6 +11,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.bookbase.bookbase.R;
+import com.bookbase.bookbase.fragments.AboutFragment;
+import com.bookbase.bookbase.fragments.BooksFragment;
+import com.bookbase.bookbase.fragments.SettingsFragment;
+import com.bookbase.bookbase.fragments.StatsFragment;
+import com.bookbase.bookbase.fragments.WishlistFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,15 +39,41 @@ public class MainActivity extends AppCompatActivity {
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener{
         @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id){ selectItem(position); }
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+
+            switch(position){
+                case 0:
+                    selectItem(new BooksFragment(), position);
+                    break;
+                case 1:
+                    selectItem(new StatsFragment(), position);
+                    break;
+                case 2:
+                    selectItem(new WishlistFragment(), position);
+                    break;
+                case 3:
+                    selectItem(new SettingsFragment(), position);
+                    break;
+                case 4:
+                    selectItem(new AboutFragment(), position);
+                    break;
+                default:
+                    break;
+
+            }
+        }
     }
 
-    private void selectItem(int position){
+    private void selectItem(Fragment fragment, int position){
 
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+        navDrawerList.setItemChecked(position, true);
+        setTitle(menuArray[position]);
+        navDrawerLayout.closeDrawer(navDrawerList);
     }
 
-    @Override
-    public void setTitle(CharSequence title){
-
-    }
 }
