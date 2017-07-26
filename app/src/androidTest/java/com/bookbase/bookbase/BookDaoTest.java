e29ee924a061bbff31268c2ffd21108a7ee62edb
Lachlan Eagling <lochie.eagling@gmail.com>
2017-07-26 19:46:35 +1000

    Add unit test class for BookDao class

---
+++ app/src/androidTest/java/com/bookbase/bookbase/BookDaoTest.java
@@ -0,0 +1,49 @@
+package com.bookbase.bookbase;
+
+import android.arch.persistence.room.Room;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.bookbase.bookbase.database.AppDatabase;
+import com.bookbase.bookbase.model.dao.BookDao;
+import com.bookbase.bookbase.model.entity.Book;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(AndroidJUnit4.class)
+public class BookDaoTest {
+
+    private BookDao bookDao;
+    private AppDatabase testDb;
+
+    @Before
+    public void createDb(){
+        Context context = InstrumentationRegistry.getTargetContext();
+        testDb = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
+        bookDao = testDb.bookDao();
+    }
+
+    @After
+    public void closeDb() throws IOException {
+        testDb.close();
+    }
+
+    @Test
+    public void insertAndAccessBook() throws Exception{
+        Book book = new Book("Test Book One", 1);
+        bookDao.insertAll(book);
+        List<Book> books = bookDao.getBooks();
+        assertThat(books.get(0).getTitle(), equalTo(book.getTitle()));
+    }
+
+}
