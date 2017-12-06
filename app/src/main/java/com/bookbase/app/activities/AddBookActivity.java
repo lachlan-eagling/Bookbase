34171dc33922fa27c0a423686c174edb92d679ba
Lachlan Eagling <lochie.eagling@gmail.com>
2017-12-06 18:45:22 +1100

    Remove unused ActionBar tab listener

--- app/src/main/java/com/bookbase/app/activities/AddBookActivity.java
+++ app/src/main/java/com/bookbase/app/activities/AddBookActivity.java
@@ -45,26 +45,6 @@ public class AddBookActivity extends AppCompatActivity {
         TabLayout tabs = (TabLayout) findViewById(R.id.tab_slider);
         tabs.setupWithViewPager(addBooksPager);
 
-//        final ActionBar actionBar = getSupportActionBar();
-//        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-//        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-//            @Override
-//            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-//                addBooksPager.setCurrentItem(tab.getPosition());
-//            }
-//
-//            @Override
-//            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-//
-//            }
-//
-//            @Override
-//            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-//                // Ignore this event.
-//            }
-//        };
-
-
     }
 
     @Override
