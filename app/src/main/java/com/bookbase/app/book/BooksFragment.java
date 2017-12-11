e04dc3c4a9da528f8af784181685f6ae4df14e86
Lachlan Eagling <lochie.eagling@gmail.com>
2017-12-11 19:57:10 +1100

    Put AddBook classes into own package

--- app/src/main/java/com/bookbase/app/book/BooksFragment.java
+++ app/src/main/java/com/bookbase/app/book/BooksFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bookbase.app.R;
+import com.bookbase.app.book.addBook.AddBookActivity;
 import com.bookbase.app.database.AppDatabase;
 import com.bookbase.app.model.entity.Book;
 
