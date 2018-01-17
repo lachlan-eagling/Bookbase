04ad2c5fa9c71ff8db841594aba5dcc306ff7265
Lachlan Eagling <lochie.eagling@gmail.com>
2018-01-17 20:35:52 +1100

    Add insert method to GenreDao

--- app/src/main/java/com/bookbase/app/model/dao/GenreDao.java
+++ app/src/main/java/com/bookbase/app/model/dao/GenreDao.java
@@ -1,6 +1,7 @@
 package com.bookbase.app.model.dao;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
 import com.bookbase.app.model.entity.Genre;
@@ -15,5 +16,8 @@ public interface GenreDao {
 
     @Query("SELECT * FROM Genre where genreId=:id")
     Genre getGenreById(int id);
+
+    @Insert
+    long insert(Genre genre);
 }
 
